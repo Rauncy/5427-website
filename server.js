5299477fbf332c7b45ed141e5da44f55fcfbf16e
Christopher Zeller <christopher.2927@gmail.com>
2017-09-15 13:38:27 -0500

    Added partial backend support

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 var http = require('http');
+var fs = require('fs');
+var server;
 
 function onRequest(request, response){
   console.log("User made a request for " + request.url);
-  response.writeHead(200, {"Context-Type": "text/plain"});
-  response.write("Data has been sent");
+  if(request.url.endsWith(".css")){
+
+  }else if(request.url.endsWith(".js"){
+
+  }else if(request.url.startsWith("/_db/")){
+
+  }else if(request.url.startsWith("/_asset/")){
+
+  }else if(request.url.startsWith("/_data/")){
+
+  }else{
+
+    response.writeHead(200, {"Context-Type" : "text/html"});
+
+    response.write(fs.readFile(`./webpages${webpages}`));
+    if(err){
+      //File doesn't exist
+      response.write(fs.readFile("./webpages/404.html"));
+    }
+  }
   response.end();
 
   //Supply correct data and pages
 }
 
-http.createServer(onRequest).listen(3000);
+server = http.createServer(onRequest);
+server.listen(3000);
 console.log("Server is now running...")
