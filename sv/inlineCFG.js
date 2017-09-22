85c5bb9151c4ff8d4ec5ee529363b39c6ed35e75
Christopher Zeller <christopher.2927@gmail.com>
2017-09-22 13:40:26 -0500

    Added some sv/ files

---
+++ sv/inlineCFG.js
@@ -0,0 +1,23 @@
+function processInlineCFGs(html){
+
+}
+
+function processText(params){
+  switch(params.type){
+    case "data":
+      //Process data
+      let path = params.src.substring(params.src.indexOf("."));
+
+      if(params.data[])
+      break;
+    case "content":
+      //Process content and if needs to go to header
+      break;
+    case "template":
+      //Insert param content into page
+      break;
+    case "fragment":
+      //Get content page and insert
+      break;
+  }
+}
