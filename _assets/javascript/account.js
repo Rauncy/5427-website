4beb140bdee73ba39137b7683b9b6874a2296f2a
Akshat <respectthegame23@gmail.com>
2017-09-25 22:21:02 -0500

    profile picture now changes in user settings; TODO:

--- _assets/javascript/account.js
+++ _assets/javascript/account.js
@@ -1,7 +1,8 @@
 function register(){
+  var email = document.getElementById('registerEmail').value;
   var pass = document.getElementById('registerPassword').value;
   var confirmPass = document.getElementById('confirmPassword').value;
-  alert(pass);
+
   if(pass != confirmPass){
       alert("Please match your passwords!");
       location.reload();
@@ -9,5 +10,9 @@ function register(){
   else if (pass.length < 6) {
       alert("Please increase the length of your password!");
       location.reload();
+  }
+  else if(email.indexOf("@") == -1){
+    alert("Please enter a valid email address!");
+    location.reload();
   }
 }
